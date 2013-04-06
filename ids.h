45deedcb8983281113841a091f56b23c19a1a9c9
Miles Lacey <mileslacey@gmail.com>
2013-04-06 01:30:43 -0700

    Initial commit

---
+++ ids.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   ids.h
+ * Author: hammy
+ *
+ * Created on April 5, 2013, 2:21 AM
+ */
+
+#ifndef __IDS_H__
+#define	__IDS_H__
+
+#ifndef IDI_MYICON
+    #define IDI_MYICON 101
+#endif
+
+#endif	/* __IDS_H__ */
+
